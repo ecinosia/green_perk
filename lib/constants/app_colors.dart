72de1704d4de98bd0712c56c228000902f863a7b
ecinosia <furkansaridiken@gmail.com>
2024-02-19 21:50:50 +0300

    feat: App colors class created.

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryGreen = const Color.fromRGBO(0, 102, 99, 1);
+  static Color textPink = const Color.fromRGBO(227, 28, 114, 1);
+  static Color primaryBlue = const Color.fromRGBO(157, 213, 207, 1);
+  static Color textGreen = const Color.fromRGBO(0, 102, 99, 1);
+  static Color white = const Color.fromRGBO(255, 255, 255, 1);
+  static Color black = const Color.fromRGBO(0, 0, 0, 1);
+}
