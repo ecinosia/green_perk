b02a854e236161e4d2e7828457b9f5a0085abc5a
ecinosia <furkansaridiken@gmail.com>
2024-02-19 22:07:33 +0300

    fix: Scrollview fixed.

--- lib/pages/registerPage/register_page.dart
+++ lib/pages/registerPage/register_page.dart
@@ -22,116 +22,118 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
-          color: AppColors.primaryGreen,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Image.asset(
-                      "lib/assets/images/left_corner.png",
-                      width: 30,
-                    ),
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      textSpan("make ", AppColors.primaryBlue),
-                      textSpan("planet ", AppColors.primaryBlue),
-                      textSpan("green ", AppColors.textPink),
-                      textSpan("again", AppColors.primaryBlue),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: Align(
-                    alignment: Alignment.topRight,
-                    child: Image.asset(
-                      "lib/assets/images/right_corner.png",
-                      width: 30,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                const InputLabel(text: 'First Name'),
-                CustomTextFormFieldWidget(
-                  controller: firstNameController,
-                ),
-                const H40CustomSizedBox(),
-                const InputLabel(text: 'Last Name'),
-                CustomTextFormFieldWidget(
-                  controller: lastNameController,
-                ),
-                const H40CustomSizedBox(),
-                const InputLabel(text: 'E-mail'),
-                CustomTextFormFieldWidget(
-                  controller: emailController,
-                ),
-                const H40CustomSizedBox(),
-                const InputLabel(text: 'Password'),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20),
-                  child: Material(
-                    elevation: 7,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    color: AppColors.primaryBlue,
-                    child: TextFormField(
-                      obscureText: true,
-                      controller: passwordController,
-                      cursorColor: AppColors.textPink,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          borderSide: BorderSide(
-                            color: AppColors.textPink,
-                          ),
-                        ),
-                        floatingLabelBehavior: FloatingLabelBehavior.auto,
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: AppColors.primaryGreen,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Image.asset(
+                        "lib/assets/images/left_corner.png",
+                        width: 30,
                       ),
                     ),
                   ),
-                ),
-                const H40CustomSizedBox(),
-                ElevatedButton(
-                  onPressed: () => context.go('/home_page'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: AppColors.textPink,
-                    minimumSize: const Size(
-                      150,
-                      50,
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        textSpan("make ", AppColors.primaryBlue),
+                        textSpan("planet ", AppColors.primaryBlue),
+                        textSpan("green ", AppColors.textPink),
+                        textSpan("again", AppColors.primaryBlue),
+                      ],
                     ),
                   ),
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      fontSize: 24,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Image.asset(
+                        "lib/assets/images/right_corner.png",
+                        width: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  const InputLabel(text: 'First Name'),
+                  CustomTextFormFieldWidget(
+                    controller: firstNameController,
+                  ),
+                  const H40CustomSizedBox(),
+                  const InputLabel(text: 'Last Name'),
+                  CustomTextFormFieldWidget(
+                    controller: lastNameController,
+                  ),
+                  const H40CustomSizedBox(),
+                  const InputLabel(text: 'E-mail'),
+                  CustomTextFormFieldWidget(
+                    controller: emailController,
+                  ),
+                  const H40CustomSizedBox(),
+                  const InputLabel(text: 'Password'),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20),
+                    child: Material(
+                      elevation: 7,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
                       color: AppColors.primaryBlue,
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: passwordController,
+                        cursorColor: AppColors.textPink,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(50),
+                            borderSide: BorderSide(
+                              color: AppColors.textPink,
+                            ),
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior.auto,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const H40CustomSizedBox(),
+                  ElevatedButton(
+                    onPressed: () => context.go('/home_page'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.textPink,
+                      minimumSize: const Size(
+                        150,
+                        50,
+                      ),
+                    ),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: AppColors.primaryBlue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
