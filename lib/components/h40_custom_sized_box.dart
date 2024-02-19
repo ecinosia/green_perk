4d88d9c016711987b1c13e27e50e249f0399810a
ecinosia <furkansaridiken@gmail.com>
2024-02-19 21:50:33 +0300

    feat: Custom sized box added

---
+++ lib/components/h40_custom_sized_box.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class H40CustomSizedBox extends StatelessWidget {
+  const H40CustomSizedBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 40,
+    );
+  }
+}
