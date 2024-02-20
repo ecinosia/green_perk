73295bb1c140d7a36e46a34589e34c816b815ddf
ecinosia <furkansaridiken@gmail.com>
2024-02-21 01:23:59 +0300

    refactor: Auth functions fixed.

--- lib/auth/auth.dart
+++ lib/auth/auth.dart
@@ -3,12 +3,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+final FirebaseAuth auth = FirebaseAuth.instance;
 void registerToFb(
     BuildContext context,
     TextEditingController fullNameController,
     TextEditingController emailController,
     TextEditingController passwordController) {
-  final FirebaseAuth auth = FirebaseAuth.instance;
   final dbRef = FirebaseFirestore.instance.collection("users");
 
   auth
@@ -20,7 +20,22 @@ void registerToFb(
       'email': emailController.text,
       'password': passwordController.text,
     }).then((res) {
-      context.go('/home_page');
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const AlertDialog(
+            title: Text("Welcome"),
+            content: Text(
+                "You successfuly registered.\nRedirecting to sign in page!"),
+          );
+        },
+      );
+      Future.delayed(
+        const Duration(seconds: 5),
+        () {
+          context.go('/sign_in_register_page/sign_in_page');
+        },
+      );
     });
   }).catchError((err) {
     showDialog(
@@ -40,4 +55,58 @@ void registerToFb(
           );
         });
   });
+}
+
+void signIn(BuildContext context, TextEditingController emailController,
+    TextEditingController passwordController) {
+  auth
+      .signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text)
+      .then(
+    (result) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return const AlertDialog(
+            title: Text("Welcome back!"),
+            content:
+                Text("You successfuly signed in.\nRedirecting to home page!"),
+          );
+        },
+      );
+      Future.delayed(
+        const Duration(seconds: 5),
+        () {
+          context.go('/home_page');
+        },
+      );
+    },
+  ).catchError((err) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Error"),
+            content: Text(err.message),
+            actions: [
+              TextButton(
+                child: const Text("Ok"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  });
+}
+
+void signOut() {
+  auth.signOut();
+}
+
+String? returnUserName() {
+  final User? user = auth.currentUser;
+  final uid = user?.uid;
+  return uid;
 }
