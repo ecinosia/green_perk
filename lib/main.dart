b58c02ba9506398317ca0f746b45ef9f0acc4b4d
ecinosia <furkansaridiken@gmail.com>
2024-02-24 16:08:31 +0300

    chore: Pages which has API key inside has been deleted.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:green_perk/firebase_options.dart';
 import 'package:green_perk/pages/accountPage/account_page.dart';
 import 'package:green_perk/pages/greenBotPage/greenperks_chatbot_page.dart';
 import 'package:green_perk/pages/greenRewardsPage/green_reward_page.dart';
@@ -16,7 +14,8 @@ import 'package:green_perk/pages/welcomePage/welcome_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //Uncomment this lines before run
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseAppCheck.instance.activate(
     androidProvider: AndroidProvider.playIntegrity,
   );
